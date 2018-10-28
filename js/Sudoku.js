a9c19a0db3baf13ad27eaf46a78cdf131d00db62
ivan <paderin94126@gmail.com>
2018-10-28 22:49:47 +0600

    теперь считывает данные и записывает в 3 массива (блоки , линнии и колонки)

--- js/Sudoku.js
+++ js/Sudoku.js
@@ -1,7 +1,8 @@
 let Sudoku = {
     matrixBlock: [],
     matrixLine: [],
-    read: function () {
+    matrixCol:[],
+    readBlock: function () {
         for (let i = 0; i < 9; i++) {
             let str = [];
             for (let j = 0; j < 9; j++) {
@@ -16,9 +17,41 @@ let Sudoku = {
         }
         console.log(Sudoku.matrixBlock);
     },
+    readLine: function () {
+        for (let i = 0; i < 9; i++) {
+            let str = [];
+            for (let j = 0; j < 9; j++) {
+                if ($(".line_" + (i + 1))[j].value === "") {
+                    str[j] = "";
+                }
+                else {
+                    str[j] = $(".line_" + (i + 1))[j].value;
+                }
+            }
+            Sudoku.matrixLine[i] = str;
+        }
+        console.log(Sudoku.matrixLine);
+    },
+    readCol: function () {
+        for (let i = 0; i < 9; i++) {
+            let str = [];
+            for (let j = 0; j < 9; j++) {
+                if ($(".col_" + (i + 1))[j].value === "") {
+                    str[j] = "";
+                }
+                else {
+                    str[j] = $(".col_" + (i + 1))[j].value;
+                }
+            }
+            Sudoku.matrixCol[i] = str;
+        }
+        console.log(Sudoku.matrixCol);
+    },
     loadEvent: function () {
         $(".read").click(function () {
-            Sudoku.read();
+            Sudoku.readBlock();
+            Sudoku.readLine();
+            Sudoku.readCol();
         })
     }
 };
